0d5364970647691e5a23280a2cd14fccbc95b4ec
JanaSundar <janasundar1997@gmail.com>
2019-09-05 03:20:20 +0000

    Restructed the JSON Response

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const app = express();
 const fetch = require('node-fetch');
 const cors = require('cors');
 
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
 app.get("/", async (req, res) => {
     const user = req.query.name;
-    await fetch(`https://www.instagram.com/${user}/?__a=1`).then((resp) => resp.json()).then(function(data) {
+    await fetch(`https://www.instagram.com/${user}/?__a=1`).then(resp => resp.json()).then(data => {
         let { username, edge_followed_by, edge_follow, edge_owner_to_timeline_media, profile_pic_url_hd } = data.graphql.user;
         res.json({
             response: true,
@@ -32,6 +33,6 @@ app.get("/", async (req, res) => {
 
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server started')
 })
